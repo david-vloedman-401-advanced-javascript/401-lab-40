14f31784f3540d6b2ea13e6a6c6f7e20c3a86df0
David Vloedman <djvloedman@gmail.com>
2020-03-08 11:25:18 -0700

    Test changes

--- src/__tests__/action.test.js
+++ src/__tests__/action.test.js
@@ -1,32 +1,9 @@
-import thunk from "redux-thunk";
-import configMockStore from "redux-mock-store";
-import * as actions from "../state/store/todo-action";
+describe('Todo application', ()=> {
+  expect('asdf', ()=>{
 
-const mockStore = configMockStore([thunk]);
-
-xdescribe("Normal Action Creators", () => {
-  it("should create a FAKE action", () => {
-    const store = mockStore({});
-    const expectedAction = [
-      {
-        type: "POST",
-        payload: "foo"
-      }
-    ];
-
-    store.dispatch(actions.post("foo"));
-    let dispatchedActions = store.getActions();
-
-    expect(dispatchedActions).toEqual(expectedAction);
   });
 });
 
-describe("async Action Creator", () => {
-  it("should create a GET action", () => {
-    const store = mockStore({ results: [] });
-    return store.dispatch(actions._getTodoItems()).then(() => {
-      let dispatchedActions = store.getActions();
-      expect(dispatchedActions[0].type).toEqual("GET");
-    });
-  });
-});
+
+
+
--- src/__tests__/reducer.test.js
+++ src/__tests__/reducer.test.js
@@ -1,16 +1,2 @@
 import reducer from "../state/store/todo-reducer";
 
-xdescribe("Our Reducer", () => {
-  it("does a GET right", () => {
-    let initialState = {};
-    let action = {
-      type: "GET",
-      payload: { name: "calvin" }
-    };
-
-    let expected = {
-      todoList: action.payload
-    };
-    expect(reducer(initialState, action)).toEqual(expected);
-  });
-});
